7afca3974686429df5a75d150105695eb42b1716
lzh0 <48125400+lzh0@users.noreply.github.com>
2020-03-12 11:57:21 +0800

    Update main.c
    add the funcation:test in the main.c

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <reg52.h>
 void fun(void);
+void test(void);
 int main(void){
 	while(1){
 		P2=0;
 		fun();
+		test();
 		P1=0XFF;
 	}
 	
@@ -11,4 +13,8 @@ int main(void){
 
 void fun(void){
 	P3=0;
+}
+
+void test(void){
+	P2=1;
 }
